16f5308a22ef5e875b2cb9abe61d1f91175e6864
KACTPO93 <ibra_uf@mail.ru>
2023-02-11 15:06:00 +0300

    Example001

---
+++ Exampler001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа проверяющая является ли 2 число квадратом 1
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число квадрат первого: ");
+int b = int.Parse(Console.ReadLine());
+int sqr = a * a;
+if (b == sqr)
+    {
+    Console.WriteLine("Правильно!");
+    }
+        else
+        {Console.WriteLine("Не правильно!");
+        }
