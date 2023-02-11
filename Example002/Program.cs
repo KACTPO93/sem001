92b35976027b354049726ec27c43ef560d1d17fc
KACTPO93 <ibra_uf@mail.ru>
2023-02-11 15:40:35 +0300

    Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int a = int.Parse(Console.ReadLine());
+if (a <= 0)Console.WriteLine("Введите корректное число");
+if (a > 7) Console.WriteLine("Введите корректное число");
+if (a == 1) Console.WriteLine("Понедельник");
+if (a == 2) Console.WriteLine("Вторник");
+if (a == 3) Console.WriteLine("Среда");
+if (a == 4) Console.WriteLine("Четверг");
+if (a == 5) Console.WriteLine("Пятница");
+if (a == 6) Console.WriteLine("Суббота");
+if (a == 7) Console.WriteLine("Воскресенье");
